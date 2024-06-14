93b519174893255fa0f98b9b058edf40a3f4f107
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-14 21:46:09 +0530

    Added Changes to sidebar item

--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 
 export default function SidebarItem({ title, icon, route }) {
     return (
-        <Link to={route} className="w-full h-12 flex pl-5 cursor-pointer text-white font-semibold text-lg items-center justify-start gap-3 hover:bg-slate-800 rounded-xl">
+        <Link
+            to={route}
+            className="w-full h-12 flex pl-5 cursor-pointer text-white font-semibold text-lg items-center justify-start gap-3 hover:bg-slate-800 rounded-xl"
+        >
             <span>{icon}</span>
             <span>{title}</span>
         </Link>
@@ -13,4 +16,5 @@ export default function SidebarItem({ title, icon, route }) {
 SidebarItem.propTypes = {
     title: PropTypes.string,
     icon: PropTypes.string,
+    route: PropTypes.string
 };
