9ad397ca3f9fa3bd9283feeecdb9f3976c9c87c1
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-06-16 00:21:10 +0530

    Added cashbook table

--- src/components/Cashbook.jsx
+++ src/components/Cashbook.jsx
@@ -1,7 +1,23 @@
 export default function Cashbook() {
     return (
-        <div className="flex w-full h-full items-start justify-start bg-background">
+        <div className="flex w-full h-full flex-col items-start justify-start gap-5 bg-background px-4 py-6">
             <h1 className="text-5xl">Cashbook</h1>
+            <div className="w-full h-auto flex flex-col items-center justify-center">
+                <table className="border-2 border-slate-100 w-full">
+                    <thead>
+                        <tr>
+                            <th className="border-2 py-3 border-slate-100">Date</th>
+                            <th className="border-2 py-3 border-slate-100">
+                                Description
+                            </th>
+                            <th className="border-2 py-3 border-slate-100">Debit</th>
+                            <th className="border-2 py-3 border-slate-100">
+                                Credit
+                            </th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </div>
     );
 }
