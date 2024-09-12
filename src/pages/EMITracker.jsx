4c9f19bdd1d9907ea3e80805a31328ad03f74675
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-09-12 21:18:33 +0530

    Changes to EMITracker Pagfe

--- src/pages/EMITracker.jsx
+++ src/pages/EMITracker.jsx
@@ -1,9 +1,28 @@
-
-
 export default function EMITracker() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="flex w-full h-full items-start justify-start gap-10 bg-background px-4 py-6 flex-col">
+            <div className="flex items-center justify-center w-full h-auto gap-6">
+                <div className="w-1/2 flex flex-col items-start justify-start gap-2">
+                    <label className="font-semibold text-sm">Date</label>
+                    <input
+                        type="date"
+                        name="name"
+                        id="name"
+                        className="w-full h-12 border-2 border-slate-100 rounded-lg p-2"
+                        required
+                    />
+                </div>
+                <div className="w-1/2 flex flex-col items-start justify-start gap-2">
+                    <label className="font-semibold text-sm">Date</label>
+                    <input
+                        type="date"
+                        name="name"
+                        id="name"
+                        className="w-full h-12 border-2 border-slate-100 rounded-lg p-2"
+                        required
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
