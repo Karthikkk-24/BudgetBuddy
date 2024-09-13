8536224e73974b5c541d9abd5c777275bf0a1cd6
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-09-13 22:39:59 +0530

    Added duration in EMI Tracker

--- src/pages/EMITracker.jsx
+++ src/pages/EMITracker.jsx
@@ -13,14 +13,17 @@ export default function EMITracker() {
                     />
                 </div>
                 <div className="w-1/2 flex flex-col items-start justify-start gap-2">
-                    <label className="font-semibold text-sm">Date</label>
-                    <input
-                        type="date"
-                        name="name"
-                        id="name"
-                        className="w-full h-12 border-2 border-slate-100 rounded-lg p-2"
-                        required
-                    />
+                    <label className="font-semibold text-sm">Duration</label>
+                    <select name="duration" className="w-full h-12 border-2 border-slate-100 rounded-lg p-2" id="duration">
+                        <option value="">Select Duration</option>
+                        <option value="3m">3 Months</option>
+                        <option value="6m">6 Months</option>
+                        <option value="9m">9 Months</option>
+                        <option value="1y">1 Year</option>
+                        <option value="18m">18 Months</option>
+                        <option value="2y">2 Years</option>
+                        <option value="3y">3 Years</option>
+                    </select>
                 </div>
             </div>
         </div>
