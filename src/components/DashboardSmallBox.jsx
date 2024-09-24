d8d30180487a466485a6b880eae737e14c6dd860
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-09-24 19:24:47 +0530

    Changes to SmallBoxz

--- src/components/DashboardSmallBox.jsx
+++ src/components/DashboardSmallBox.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export default function DashboardSmallBox({ data, name }) {
     return (
-        <div className="shadow-xl w-1/4 flex flex-col gap-3 items-center justify-center rounded-xl h-52">
+        <div className="shadow-xl w-1/4 flex flex-col gap-3 items-center justify-center rounded-xl h-52 border-2">
             <h2 className="font-bold text-primary text-3xl text-center">
                 {data}
             </h2>
